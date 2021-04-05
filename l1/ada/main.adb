df8509550189ae7a55252b2ca2e2a976d32ba6fc
Paulaxx <palinkagr@gmail.com>
2021-04-05 21:58:11 +0200

    almost done

--- l1/ada/main.adb
+++ l1/ada/main.adb
@@ -14,7 +14,25 @@ procedure main is
       Element_Type => Integer);
    use Integer_Vectors;
 
+   -- package type of data(structure)
+   type Packagee is record
+        Id : Integer;
+        visited_vertices : Vector;
+   end record;
+
+   -- verticle type of data(structure)
+   type Verticle is record
+      Id : Integer;
+      packages: Vector;
+      where_to_go: Vector;
+      empty: Integer;
+   end record;
+
    edges: array (0..Parameters.n-2) of Vector;
+   all_packages: array(0..Parameters.k-1) of Packagee;
+   all_verticles: array(0..Parameters.n-1) of Verticle;
+   empty : Vector;
+   v: Verticle;
 
    --Checking if exist
    function exist(x, y, size : in integer) return integer is
@@ -27,6 +45,20 @@ procedure main is
       end loop;
       return 0;
    end exist;
+
+   function exist2(x, y, size : in integer) return integer is
+   i : integer := 0;
+   begin
+      if x+1 = y then
+         return 1;
+      end if;
+      for i in 0..size loop
+         if addition(i,0) = x and addition(i,1) = y then
+            return 1;
+         end if;
+      end loop;
+      return 0;
+   end exist2;
 
    -- Random edges generation
    procedure randomN is
@@ -73,6 +105,94 @@ procedure main is
       end loop;
    end add_vectors;
 
+   -- thread of the printer
+   task Printer is
+      entry Go (Msg: String);
+   end Printer ;
+
+   task body Printer is
+   begin
+      loop
+         accept Go (Msg : String) do
+            put(Msg);
+            delay 1.0;
+         end Go;
+      end loop;
+   end Printer;
+
+
+
+   -- thread of every vertex
+   task type Taskvertex is
+      entry Starttask (Vert: Verticle);
+      entry Channel (Pack: Packagee);
+   end Taskvertex;
+
+   --array with tasks
+   tasks: array(0..Parameters.n-1) of Taskvertex;
+
+   task body Taskvertex is 
+   ver: Verticle;
+   numm, where : Integer;
+   type randRange2 is new Integer range 1..Parameters.n-1;
+   package Rand_Int is new ada.numerics.discrete_random(randRange2);
+   use Rand_Int;
+   gen2 : Generator;
+   begin
+      loop
+         select
+            accept Starttask (Vert: Verticle) do
+               ver := Vert;
+            end Starttask;
+         or
+            accept Channel (Pack: Packagee) do
+               Printer.Go("pakiet " & Integer'Image(Pack.id) & " jest w wierzcholku " & Integer'Image(ver.id));
+               numm := Integer(random(gen2));
+               while exist2(ver.id, numm, Parameters.d-1) = 0 loop
+                  numm := Integer(random(gen2));
+               end loop;
+               where := ver.where_to_go(numm);
+               tasks(where).Channel(Pack);
+            end Channel;
+         end select;
+      end loop;
+   end Taskvertex;
+
+
+   -- thread of the sender
+   task Sender is
+      entry StartSending;
+   end Sender ;
+
+   task body Sender is
+   begin
+         put("sender wysyla");
+         accept StartSending do
+            put("sender accept");
+            for i2 in 0..Parameters.k-1 loop
+               tasks(0).Channel(all_packages(i2));
+               put("sender wyslal " & Integer'Image(all_packages(i2).id));
+               delay 2.0;
+            end loop;
+         end StartSending; 
+   end Sender;
+
+
+   --thread of the receiver
+   task Receiver is
+      entry Receive (Pack: Packagee);
+   end Receiver;
+
+   task body Receiver is
+   begin
+      loop
+         accept Receive (Pack: Packagee) do
+            Printer.Go("pakiet " & Integer'Image(Pack.id) & " zostal odebrany");
+            delay 2.0;
+         end Receive;
+      end loop;
+   end Receiver;
+
 
 begin
    randomN;
@@ -86,4 +206,26 @@ begin
          new_line(1);
       end loop;
    end loop;
+
+   for i in 0..Parameters.k-1 loop
+      all_packages(i).id := i;
+      all_packages(i).visited_vertices := empty;
+   end loop;
+
+   for i in 0..Parameters.n-1 loop
+      all_verticles(i).id := i;
+      all_verticles(i).packages := empty;
+      all_verticles(i).where_to_go := edges(i);
+      all_verticles(i).empty := 1;
+   end loop;
+
+   Sender.StartSending;
+
+   for i in 0..Parameters.n-1 loop
+      v := all_verticles(i);
+      tasks(i).Starttask(v);
+   end loop;
+
+   Printer.Go("Main");
+   Receiver.Receive(all_packages(0));
 end main;
