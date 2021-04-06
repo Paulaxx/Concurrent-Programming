275eb286e8db391347396621c9b86812b11991c7
Paulaxx <palinkagr@gmail.com>
2021-04-06 22:39:16 +0200

    finally

--- l1/ada/main.adb
+++ l1/ada/main.adb
@@ -16,8 +16,8 @@ procedure main is
 
    -- package type of data(structure)
    type Packagee is record
-        Id : Integer;
-        visited_vertices : Vector;
+      Id : Integer;
+      visited_vertices : Vector;
    end record;
 
    -- verticle type of data(structure)
@@ -28,7 +28,7 @@ procedure main is
       empty: Integer;
    end record;
 
-   edges: array (0..Parameters.n-2) of Vector;
+   edges: array (0..Parameters.n-1) of Vector;
    all_packages: array(0..Parameters.k-1) of Packagee;
    all_verticles: array(0..Parameters.n-1) of Verticle;
    empty : Vector;
@@ -36,7 +36,7 @@ procedure main is
 
    --Checking if exist
    function exist(x, y, size : in integer) return integer is
-   i : integer := 0;
+      i : integer := 0;
    begin
       for i in 0..size loop
          if addition(i,0) = x and addition(i,1) = y then
@@ -47,7 +47,7 @@ procedure main is
    end exist;
 
    function exist2(x, y, size : in integer) return integer is
-   i : integer := 0;
+      i : integer := 0;
    begin
       if x+1 = y then
          return 1;
@@ -62,13 +62,13 @@ procedure main is
 
    -- Random edges generation
    procedure randomN is
-   type randRange is new Integer range 0..Parameters.n-1;
-   package Rand_Int is new ada.numerics.discrete_random(randRange);
-   use Rand_Int;
-   gen : Generator;
-   num1 : integer;
-   num2 : integer;
-   i : integer := 0;
+      type randRange is new Integer range 0..Parameters.n-1;
+      package Rand_Int is new ada.numerics.discrete_random(randRange);
+      use Rand_Int;
+      gen : Generator;
+      num1 : integer;
+      num2 : integer;
+      i : integer := 0;
    begin
       reset(gen);
       while i < Parameters.d loop
@@ -84,7 +84,7 @@ procedure main is
 
    -- Creating vector for one vertex
    function make_vector(i: in integer) return vector is
-   V : Vector;
+      V : Vector;
    begin
       V.append(i+1);
       for j in 0..Parameters.d-1 loop
@@ -97,15 +97,15 @@ procedure main is
 
    -- Adding vectors to an array
    procedure add_vectors is
-   V2 : Vector;
+      V2 : Vector;
    begin
-      for j in 0..Parameters.n-2 loop
+      for j in 0..Parameters.n-1 loop
          V2 := make_vector(j);
          edges(j) := V2;
       end loop;
    end add_vectors;
 
-   -- thread of the printer
+   --thread of the printer
    task Printer is
       entry Go (Msg: String);
    end Printer ;
@@ -113,13 +113,80 @@ procedure main is
    task body Printer is
    begin
       loop
-         accept Go (Msg : String) do
-            put(Msg);
-            delay 1.0;
-         end Go;
+         select
+            accept Go (Msg : String) do
+               put_line(Msg);
+               delay 1.0;
+            end Go;
+         or
+            terminate;
+         end select;
       end loop;
    end Printer;
 
+
+   -- thread of printing statistics
+   task Statistics is
+      entry Stat;
+   end Statistics;
+
+   task body Statistics is
+   begin
+      accept Stat do
+         null;
+      end Stat;
+
+      put_line(" ");
+      put_line("Obsluzone pakiety:");
+      for i in 0..Parameters.n-1 loop
+         put("Wierzcholek " & Integer'Image(i) & " : ");
+         for j in 0..Integer(all_verticles(i).packages.Length)-1 loop
+            put(Integer'Image(all_verticles(i).packages(j)));
+            put(" ");
+         end loop;
+         put_line(" ");
+      end loop;
+
+      put_line(" ");
+      put_line("Odwiedzone wierzcholki:");
+      for i in 0..Parameters.k-1 loop
+         put("Pakiet " & Integer'Image(i) & " : ");
+         for j in 0..Integer(all_packages(i).visited_vertices.Length)-1 loop
+            put(Integer'Image(all_packages(i).visited_vertices(j)));
+            put(" ");
+         end loop;
+         put_line(" ");
+      end loop;
+
+   end Statistics;
+
+   --thread of the receiver
+   task Receiver is
+      entry Start2;
+      entry Receive (Pack: Packagee);
+   end Receiver;
+
+   task body Receiver is
+   begin
+       loop
+         select
+            accept Start2 do
+               null;
+            end Start2;
+         or
+            accept Receive (Pack: Packagee) do
+               Printer.Go("pakiet " & Integer'Image(Pack.id) & " zostal odebrany");
+               delay 2.0;
+               if Pack.id = Parameters.k-1 then
+                  delay 2.0;
+                  Statistics.Stat;
+               end if;
+            end Receive;
+         or
+            terminate;
+         end select;
+      end loop;
+   end Receiver;
 
 
    -- thread of every vertex
@@ -132,12 +199,13 @@ procedure main is
    tasks: array(0..Parameters.n-1) of Taskvertex;
 
    task body Taskvertex is 
-   ver: Verticle;
-   numm, where : Integer;
-   type randRange2 is new Integer range 1..Parameters.n-1;
-   package Rand_Int is new ada.numerics.discrete_random(randRange2);
-   use Rand_Int;
-   gen2 : Generator;
+      ver: Verticle;
+      numm, where : Integer;
+      --type randRange2 is new Integer range 0..Parameters.n-1;
+      package Rand_Int is new ada.numerics.discrete_random(Integer);
+      use Rand_Int;
+      gen2 : Generator;
+      tmppack : Packagee;
    begin
       loop
          select
@@ -146,14 +214,32 @@ procedure main is
             end Starttask;
          or
             accept Channel (Pack: Packagee) do
-               Printer.Go("pakiet " & Integer'Image(Pack.id) & " jest w wierzcholku " & Integer'Image(ver.id));
-               numm := Integer(random(gen2));
-               while exist2(ver.id, numm, Parameters.d-1) = 0 loop
-                  numm := Integer(random(gen2));
-               end loop;
-               where := ver.where_to_go(numm);
-               tasks(where).Channel(Pack);
+               tmppack := Pack;
+               all_verticles(ver.id).packages.append(Pack.id);
+               all_packages(Pack.id).visited_vertices.append(ver.id);
             end Channel;
+
+            if ver.id = Parameters.n-1  then
+               Printer.Go("pakiet " & Integer'Image(tmppack.id) & " jest w wierzcholku " & Integer'Image(ver.id));
+               Receiver.Receive(tmppack);
+            else
+               Printer.Go("pakiet " & Integer'Image(tmppack.id) & " jest w wierzcholku " & Integer'Image(ver.id));
+
+               if ver.where_to_go.Length = 1 then
+                  numm := ver.id + 1;
+                  where := ver.where_to_go(0);
+               else
+                  numm := Integer(random(gen2)) mod Integer(ver.where_to_go.Length);
+                  where := ver.where_to_go(numm);
+               end if;
+               tasks(where).Channel(tmppack);
+
+            end if;
+
+            delay 1.0;
+         or
+            terminate;
+
          end select;
       end loop;
    end Taskvertex;
@@ -166,32 +252,14 @@ procedure main is
 
    task body Sender is
    begin
-         put("sender wysyla");
          accept StartSending do
-            put("sender accept");
-            for i2 in 0..Parameters.k-1 loop
-               tasks(0).Channel(all_packages(i2));
-               put("sender wyslal " & Integer'Image(all_packages(i2).id));
-               delay 2.0;
-            end loop;
+            null;
          end StartSending; 
+         for i2 in 0..Parameters.k-1 loop
+               tasks(0).Channel(all_packages(i2));
+               delay 1.0;
+         end loop;
    end Sender;
-
-
-   --thread of the receiver
-   task Receiver is
-      entry Receive (Pack: Packagee);
-   end Receiver;
-
-   task body Receiver is
-   begin
-      loop
-         accept Receive (Pack: Packagee) do
-            Printer.Go("pakiet " & Integer'Image(Pack.id) & " zostal odebrany");
-            delay 2.0;
-         end Receive;
-      end loop;
-   end Receiver;
 
 
 begin
@@ -219,13 +287,14 @@ begin
       all_verticles(i).empty := 1;
    end loop;
 
-   Sender.StartSending;
+   Printer.Go(" ");
 
    for i in 0..Parameters.n-1 loop
       v := all_verticles(i);
       tasks(i).Starttask(v);
    end loop;
 
-   Printer.Go("Main");
-   Receiver.Receive(all_packages(0));
+   Sender.StartSending;
+   Receiver.Start2;
+
 end main;
