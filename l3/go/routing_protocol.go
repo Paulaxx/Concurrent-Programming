5e11e94120d3cab8e55efc0777e2c64fbeb9d6b8
Paulaxx <palinkagr@gmail.com>
2021-05-16 17:58:40 +0200

    add goroutines for sender and routing table keeper

--- l3/go/routing_protocol.go
+++ l3/go/routing_protocol.go
@@ -5,12 +5,39 @@ import (
 	"math"
 	"math/rand"
 	"sort"
+	"time"
 )
 
 const (
 	n = 5
 	d = 2
 )
+
+type pair struct {
+	verticle_id int
+	cost        int
+}
+
+type packet struct {
+	id      int
+	channel chan []pair
+}
+
+type vertex struct {
+	id          int
+	where_to_go []int
+}
+
+type routing_table_item struct {
+	nexthop int
+	cost    int
+	changed bool
+}
+
+type what_to_change struct {
+	id   int
+	item routing_table_item
+}
 
 func exist(v1 int, v2 int, tab [d][2]int) bool {
 
@@ -53,6 +80,52 @@ func print_edges(edges [][]int) {
 	}
 }
 
+func contains(neighbors []int, find int) bool {
+	for _, element := range neighbors {
+		if element == find {
+			return true
+		}
+	}
+	return false
+}
+
+func store_routing_table(id int, routing_table []routing_table_item, change_changed <-chan int, get_actual_routing_table <-chan int,
+	routing_table_chan chan []routing_table_item, done chan bool, replacement <-chan what_to_change) {
+
+	for {
+		select {
+		case j := <-change_changed:
+			routing_table[j].changed = !routing_table[j].changed
+			//done <- true
+		case <-get_actual_routing_table:
+			routing_table_chan <- routing_table
+		case repl := <-replacement:
+			routing_table[repl.id] = repl.item
+			done <- true
+		}
+	}
+}
+
+func sender(v vertex, get_actual_routing_table chan int, routing_table_chan <-chan []routing_table_item, receivers []chan []pair, change_changed chan int) {
+
+	for {
+		get_actual_routing_table <- 0
+		routing_table := <-routing_table_chan
+		var packet []pair
+		for id, element := range routing_table {
+			if element.changed == true {
+				pair := pair{verticle_id: id, cost: routing_table[id].cost}
+				packet = append(packet, pair)
+				change_changed <- id
+			}
+		}
+		for _, element := range v.where_to_go {
+			receivers[element] <- packet
+		}
+		time.Sleep(2000 * time.Millisecond)
+	}
+}
+
 func main() {
 
 	additional := make([]int, n)
@@ -88,4 +161,65 @@ func main() {
 		which[from2]++
 	}
 	print_edges(edges)
+
+	// kanaly do wysylania pakietow do receivera i odbierania tych pakietow przez receivera
+	var receiver [n]chan []pair
+	for i := range receiver {
+		receiver[i] = make(chan []pair, n*n)
+	}
+
+	// kanaly do wysylania aktualnego routing table
+	var return_actual_routing_table [n]chan []routing_table_item
+	for i := range return_actual_routing_table {
+		return_actual_routing_table[i] = make(chan []routing_table_item, n*n)
+	}
+
+	// kanaly do zmiany parametru changed na otrzymanej pozycji
+	var change_changed [n]chan int
+	for i := range change_changed {
+		change_changed[i] = make(chan int, n*n)
+	}
+
+	// kanaly do wyslania zapytania o aktualna routing table
+	var get_actual_routing_table [n]chan int
+	for i := range get_actual_routing_table {
+		get_actual_routing_table[i] = make(chan int, n*n)
+	}
+
+	var done [n]chan bool
+	for i := range done {
+		done[i] = make(chan bool, n*n)
+	}
+
+	var channel_to_change [n]chan what_to_change
+	for i := range channel_to_change {
+		channel_to_change[i] = make(chan what_to_change, n*n)
+	}
+
+	var nexthop, cost int
+	var changed bool
+	for i := 0; i < n; i++ {
+		routing_table := make([]routing_table_item, n)
+		for j := 0; j < n; j++ {
+			if i != j {
+				if contains(edges[i], j) {
+					nexthop = j
+					cost = 1
+				} else {
+					if i < j {
+						nexthop = i + 1
+						cost = j - i
+					} else {
+						nexthop = i - 1
+						cost = i - j
+					}
+				}
+				changed = true
+				item := routing_table_item{nexthop: nexthop, cost: cost, changed: changed}
+				routing_table[j] = item
+			}
+		}
+		go store_routing_table(i, routing_table, change_changed[i], get_actual_routing_table[i], return_actual_routing_table[i], done[i], channel_to_change[i])
+	}
+
 }
